bb58213cb3c32d25929076007397e9cc88b0c20a
vvchoo <vvchoo@users.noreply.github.com>
2019-07-17 18:36:46 -0400

    Create snap polls.R

---
+++ snap polls.R
@@ -0,0 +1,69 @@
+library(shiny)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(plotly)
+library(rsconnect)
+setwd("C:/Users/Vera/Desktop/TRIP/WEB/SNAP POLLS")
+
+####### create snap poll x ########
+fac17<-read.csv("TRIP_FacultySurvey_2017_Intl_Clean_1.1.csv",stringsAsFactor=FALSE)
+names(fac17)<-tolower(names(fac17))
+Qs<-read.csv("TRIP_FS2017_US_SnapPoll_Listofquestions.csv",stringsAsFactors=FALSE)
+Qs<-Qs[-3]
+Qs$Question<-tolower(Qs$Question)
+#i<-grep(paste0(Qs$Question,collapse="|"),names(fac17))
+#snap_10<-fac17[i]
+
+#write.csv(snap_10,"TRIP_SnapPoll10_1.0.0.csv",fileEncoding="UTF-8")
+
+###### read in snap polls ########
+### should read in individually, as needed ###
+i<-1
+for(i in 1:10){
+  assign(paste("snap",i,sep="_"),read.csv(paste("TRIP_SnapPoll",i,"_1.0.0.csv",sep=""),stringsAsFactors=FALSE))
+  i<-i+1
+}
+
+#######################################################
+#                   USER INTERFACE                    #
+#######################################################
+ui <- fluidPage(
+  titlePanel("Journal Article Database"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("dataSelect","Choose snap poll..",choices=c("Select...","Snap Poll I"="snap_1",
+                                                              "Snap Poll II"="snap_2",
+                                                              "Snap Poll III"="snap_3",
+                                                              "Snap Poll IV"="snap_4",
+                                                              "Snap Poll V"="snap_5",
+                                                              "Snap Poll VI"="snap_6",
+                                                              "Snap Poll VII"="snap_7",
+                                                              "Snap Poll VIII"="snap_8",
+                                                              "Snap Poll IX"="snap_9",
+                                                              "Snap Poll X"="snap_10"))),
+    mainPanel(plotOutput("graph"),tableOutput("questions"))),
+  tags$style(type="text/css", ".shiny-output-error{visibility: hidden;}", ".shiny-output-error:before{visibility:hidden;}")
+)
+
+#######################################################
+#                       SERVER                        #
+#######################################################
+server <- function(input, output, session) {
+  observeEvent(input$dataSelect, {
+    if(input$dataSelect=="Select..."){
+    }
+  })
+  ## create graph ##
+  output$graph<-renderPlot({
+    ggplot(get(input$dataSelect),aes(x=qg_104)) +
+       geom_bar(stat="count") +
+       theme(axis.text.x=element_text(angle=45))
+  })
+  ## list of questions ##
+  output$questions<-renderTable({
+    head(get(input$dataSelect))
+  })
+}
+
+shinyApp(ui = ui, server = server)
