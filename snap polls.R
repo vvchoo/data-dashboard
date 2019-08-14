b665716127ee66e2c73945ded4eed3e96a7fcb9c
vvchoo <vvchoo@users.noreply.github.com>
2019-08-14 16:00:52 -0400

    Update snap polls.R

--- snap polls.R
+++ snap polls.R
@@ -7,7 +7,7 @@ library(rsconnect)
 setwd("C:/Users/Vera/Desktop/TRIP/WEB/SNAP POLLS")
 
 ####### create snap poll x ########
-fac17<-read.csv("TRIP_FacultySurvey_2017_Intl_Clean_1.1.csv",stringsAsFactor=FALSE)
+fac17<-read.csv("TRIP_FacultySurvey_2017_US_Clean_1.1.csv",stringsAsFactor=FALSE)
 names(fac17)<-tolower(names(fac17))
 Qs<-read.csv("TRIP_FS2017_US_SnapPoll_Listofquestions.csv",stringsAsFactors=FALSE)
 Qs$Question<-tolower(Qs$Question)
@@ -15,7 +15,7 @@ i<-grep(paste0(Qs$Question,collapse="|"),names(fac17))
 snap_10<-cbind(fac17[3],fac17[i])
 #names(snap_10)[names(snap_10) %in% Qs$Question]<-Qs$Question_text
 
-#write.csv(snap_10,"TRIP_SnapPoll10_1.0.0.csv",fileEncoding="UTF-8")
+write.csv(snap_10,"TRIP_SnapPoll10_1.0.0.csv",fileEncoding="UTF-8")
 
 ###### read in snap polls ########
 ### should read in individually, as needed ###
@@ -32,16 +32,16 @@ ui <- fluidPage(
   titlePanel("Snap Poll (testing ver.)"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("dataSelect","Choose snap poll..",choices=c("Select...","Snap Poll I"="snap_1",
-                                                              "Snap Poll II"="snap_2",
-                                                              "Snap Poll III"="snap_3",
-                                                              "Snap Poll IV"="snap_4",
-                                                              "Snap Poll V"="snap_5",
-                                                              "Snap Poll VI"="snap_6",
-                                                              "Snap Poll VII"="snap_7",
-                                                              "Snap Poll VIII"="snap_8",
-                                                              "Snap Poll IX"="snap_9",
-                                                              "Snap Poll X"="snap_10"))),
+      radioButtons("dataSelect","Choose snap poll..",choices=c("Snap Poll I: Syria, Ukraine, and the U.S. Defense Budget"="snap_1",
+                                                    "Snap Poll II: Ukraine, Energy, and the Middle East"="snap_2",
+                                                    "Snap Poll III: Seven Questions on Current Global Issues for IR Scholars"="snap_3",
+                                                    "Snap Poll IV: Ten Questions on Current Global Issues for IR Scholars"="snap_4",
+                                                    "Snap Poll V: Proposed Nuclear Agreement with Iran"="snap_5",
+                                                    "Snap Poll VI: Greece, Migration Crisis, Trade Agreements, and FIFA"="snap_6",
+                                                    "Snap Poll VII: South China Sea, Refugee Crisis, and Iran Deal"="snap_7",
+                                                    "Snap Poll VIII: 2016 Presidential Campaign, Zika, and Terrorism in the Middle East"="snap_8",
+                                                    "Snap Poll IX: U.S. Foreign Policy and the 2016 Presidential Election"="snap_9",
+                                                    "Snap Poll X (Embedded in 2017 Faculty Survey)"="snap_10"))), # updates go here
     mainPanel(
       tabsetPanel(type="tabs",
                   tabPanel("Questions",tableOutput("questions")),
@@ -67,7 +67,9 @@ server <- function(input, output, session) {
   ## list of questions ##
   output$questions<-renderTable({
     head(get(input$dataSelect))[1:5]
+    #head(Qs$Question_text)[1:5]
   })
 }
 
 shinyApp(ui = ui, server = server)
+
