5c91fa036416b85dcfdb2264f594489d4073b07f
vvchoo <vvchoo@users.noreply.github.com>
2019-08-27 15:17:32 -0400

    Update app ver 1.R

--- app ver 1.R
+++ app ver 1.R
@@ -1,5 +1,10 @@
-
-
+library(shiny)
+library(dplyr)
+library(skimr)
+library(tidyr)
+library(ggplot2)
+library(plotly)
+library(rsconnect)
 
 
 #######################################################
@@ -11,36 +16,67 @@ ui <- fluidPage(
                  tabPanel("Questions",
                           br(),
                           sidebarPanel(
-                            selectInput("dataSelect","Choose Survey Year:",choices=c("2004 Faculty Survey"="qid_2004","2006 Faculty Survey"="qid_2006","2008 Faculty Survey"="qid_2008","2011 Faculty Survey"="qid_2011","2014 Faculty Survey"="qid_2014","2017 Faculty Survey"="qid_2017","All years (selected questions)"="qid_questions"))), # updates go here
-                          mainPanel("Questions",uiOutput("questions"),textOutput("test"))),
+                            selectInput("dataSelect","Choose Survey Year:",choices=c("2004 Faculty Survey"=3,"2006 Faculty Survey"=4,"2008 Faculty Survey"=5,"2011 Faculty Survey"=6,"2014 Faculty Survey"=7,"2017 Faculty Survey"=8,"All years (selected questions)"=1))), # updates go here
+                          mainPanel(uiOutput("questions"),textOutput("print"))),
                  tabPanel("Graph",
                           br(),
                           sidebarPanel(
                             selectInput("test","test",c(1:10)),
                             actionButton("return","Return to question list")),
-                          mainPanel("Graph",plotOutput("graph")))),
+                          mainPanel(plotOutput("graph")))),
   tags$style(type="text/css", ".shiny-output-error{visibility: hidden;}", ".shiny-output-error:before{visibility:hidden;}"))
 
 #######################################################
 #                       SERVER                        #
 #######################################################
 server <- function(input, output, session) {
+
+###################### Q U E S T I O N S ######################
+  
   observeEvent(input$dataSelect, {
     if(input$dataSelect=="All years (selected questions)"){
       all_df<-new_surveys
-      list<-lapply(3:8, function(x) unlist(strsplit(qid_overtime[as.numeric(text$test),x],":")))
+      list<-lapply(3:8, function(x) unlist(strsplit(qid_overtime[as.numeric(dataStore$test),x],":")))
       names<-list(names(fac04),names(fac06),names(fac08),names(fac11),names(fac14),names(fac17))
       matched_list<-lapply(1:6, function(x) ifelse(list[[x]]!="",grep(paste(list[[x]],collapse="|"),names(all_df[[x]]),NA)))
       for(x in 1:6){ifelse(length(matched_list[[x]])==0,all_df[[x]]<-NA,
-                           ifelse(length(matched_list[[x]])==1,all_df[[x]]<-all_df[[x]][,matched_list[[x]][1]],
-                           ifelse(length(matched_list[[x]])==2,all_df[[x]]<-all_df[[x]][,matched_list[[x]][1]:matched_list[[x]][2]])))}
+                    ifelse(length(matched_list[[x]])==1,all_df[[x]]<-all_df[[x]][,matched_list[[x]][1]],
+                    ifelse(length(matched_list[[x]])==2,all_df[[x]]<-all_df[[x]][,matched_list[[x]][1]:matched_list[[x]][2]])))}
     }
   })
   
+  ## list of questions ##
+  output$questions<-renderUI({
+    lapply(1:10, function(x) fluidRow(actionLink(paste0("btn_",x),qid_overtime[qid_overtime[as.numeric(input$dataSelect)]!="",1][x]), br()))
+  })
+  
+  dataStore<-reactiveValues(dataLoc=NULL,dataNum=NULL)
+  observe({
+    input_btn<-paste0("btn_", 1:10)
+    lapply(input_btn, function(x) observeEvent(input[[x]],{
+      i <- as.numeric(sub("btn_", "", x))
+      dataStore$dataNum <- i
+      dataStore$dataLoc<-qid_2004[as.numeric(dataStore$dataNum),as.numeric(input$dataSelect)]
+      updateTabsetPanel(session, "Questions","Graph")}))
+  })
+  
+  output$print<-renderText({paste(dataStore$dataLoc)})
+  
+  df<-reactive({ifelse(input$dataSelect==3, df<-new_surveys[[1]],
+                       ifelse(input$dataSelect==4, df<-new_surveys[[2]],
+                              ifelse(input$dataSelect==5, df<-new_surveys[[3]],
+                                     ifelse(input$dataSelect==6, df<-new_surveys[[4]],
+                                            ifelse(input$dataSelect==7, df<-new_surveys[[5]],
+                                                   ifelse(input$dataSelect==8, df<-new_surveys[[6]]))))))
+  })
+
+######################  G R A P H ######################
+  
   ## create graph ##
   output$graph<-renderPlot({
-    ggplot(fac04,aes(x=fac04[[grep(tbgraph$test, names(fac04))]])) +
+    ggplot(df(),aes_string(x=df()[[grep(dataStore$dataLoc, names(df()))]])) +
       geom_bar(stat="count",fill="#ffbfd7") +
+      geom_text(aes(label=count)) +
       theme(axis.text.x=element_text(angle=45)) + 
       theme_bw()
   })
@@ -48,23 +84,6 @@ server <- function(input, output, session) {
   ## return to questions list ##
   observeEvent(input$return, {
     updateTabsetPanel(session,"Questions","Questions")
-  })
-  
-  ## list of questions ##
-  output$questions<-renderUI({
-    lapply(1:10, function(x) fluidRow(actionLink(paste0("btn_",x),get(input$dataSelect)[[1]][x]), br()))
-  })
-  
-  text<-reactiveValues(test = "Hello")
-  tbgraph<-reactiveValues(test=NULL)
-  
-  observe({
-    input_btn<-paste0("btn_", 1:10)
-    lapply(input_btn, function(x) observeEvent(input[[x]],{
-                                    i <- as.numeric(sub("btn_", "", x))
-                                    text$test <- i
-                                    tbgraph$test<-qid_2004[as.numeric(text$test),3]
-                                    updateTabsetPanel(session, "Questions","Graph")}))
   })
 }
 
