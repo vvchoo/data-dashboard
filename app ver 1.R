3fe0f65c4816cc7ba366a434ee3ebb255950b0f5
vvchoo <vvchoo@users.noreply.github.com>
2019-08-28 10:09:16 -0400

    Update app ver 1.R

--- app ver 1.R
+++ app ver 1.R
@@ -32,22 +32,11 @@ ui <- fluidPage(
 server <- function(input, output, session) {
 
 ###################### Q U E S T I O N S ######################
-  
-  observeEvent(input$dataSelect, {
-    if(input$dataSelect=="All years (selected questions)"){
-      all_df<-new_surveys
-      list<-lapply(3:8, function(x) unlist(strsplit(qid_overtime[as.numeric(dataStore$test),x],":")))
-      names<-list(names(fac04),names(fac06),names(fac08),names(fac11),names(fac14),names(fac17))
-      matched_list<-lapply(1:6, function(x) ifelse(list[[x]]!="",grep(paste(list[[x]],collapse="|"),names(all_df[[x]]),NA)))
-      for(x in 1:6){ifelse(length(matched_list[[x]])==0,all_df[[x]]<-NA,
-                    ifelse(length(matched_list[[x]])==1,all_df[[x]]<-all_df[[x]][,matched_list[[x]][1]],
-                    ifelse(length(matched_list[[x]])==2,all_df[[x]]<-all_df[[x]][,matched_list[[x]][1]:matched_list[[x]][2]])))}
-    }
-  })
+  qid<-reactive({qid_overtime[qid_overtime[as.numeric(input$dataSelect)]!="",c(1,as.numeric(input$dataSelect))]})
   
   ## list of questions ##
   output$questions<-renderUI({
-    lapply(1:10, function(x) fluidRow(actionLink(paste0("btn_",x),qid_overtime[qid_overtime[as.numeric(input$dataSelect)]!="",1][x]), br()))
+    lapply(1:10, function(x) fluidRow(actionLink(paste0("btn_",x),qid()[,1][x]), br()))
   })
   
   dataStore<-reactiveValues(dataLoc=NULL,dataNum=NULL)
@@ -56,27 +45,26 @@ server <- function(input, output, session) {
     lapply(input_btn, function(x) observeEvent(input[[x]],{
       i <- as.numeric(sub("btn_", "", x))
       dataStore$dataNum <- i
-      dataStore$dataLoc<-qid_2004[as.numeric(dataStore$dataNum),as.numeric(input$dataSelect)]
+      dataStore$dataLoc<-paste(unlist(strsplit(qid()[as.numeric(dataStore$dataNum),2],":")),collapse="|")
       updateTabsetPanel(session, "Questions","Graph")}))
   })
   
   output$print<-renderText({paste(dataStore$dataLoc)})
   
-  df<-reactive({ifelse(input$dataSelect==3, df<-new_surveys[[1]],
-                       ifelse(input$dataSelect==4, df<-new_surveys[[2]],
-                              ifelse(input$dataSelect==5, df<-new_surveys[[3]],
-                                     ifelse(input$dataSelect==6, df<-new_surveys[[4]],
-                                            ifelse(input$dataSelect==7, df<-new_surveys[[5]],
-                                                   ifelse(input$dataSelect==8, df<-new_surveys[[6]]))))))
+  df<-reactive({data.frame(ifelse(input$dataSelect==3, df<-new_surveys[1],
+                           ifelse(input$dataSelect==4, df<-new_surveys[2],
+                           ifelse(input$dataSelect==5, df<-new_surveys[3],
+                           ifelse(input$dataSelect==6, df<-new_surveys[4],
+                           ifelse(input$dataSelect==7, df<-new_surveys[5],
+                           ifelse(input$dataSelect==8, df<-new_surveys[6])))))))
   })
 
 ######################  G R A P H ######################
   
   ## create graph ##
   output$graph<-renderPlot({
-    ggplot(df(),aes_string(x=df()[[grep(dataStore$dataLoc, names(df()))]])) +
+    ggplot(data=df(),aes_string(x=df()[[grep(dataStore$dataLoc, names(df()))]])) +
       geom_bar(stat="count",fill="#ffbfd7") +
-      geom_text(aes(label=count)) +
       theme(axis.text.x=element_text(angle=45)) + 
       theme_bw()
   })
