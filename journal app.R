6cb42c7bbb11574bae70a8ee323c5986d9e2d4b3
vvchoo <vvchoo@users.noreply.github.com>
2019-10-08 10:20:34 -0400

    Update journal app.R

--- journal app.R
+++ journal app.R
@@ -4,14 +4,19 @@ library(tidyr)
 library(ggplot2)
 library(plotly)
 library(rsconnect)
+library(base64enc)
+library(RCurl)
+library(htmlwidgets)
+
 #rsconnect::setAccountInfo(name='vvchoo', token='20318BE8E51FD829619E1D7095A496CB', secret='VUQ9WQai6Vt1CftmuLpcaxg5j3pHNgYReyWhCGZK')
-JAD<-read.csv("https://www.dropbox.com/s/1w5wlmur5x3x7gq/TRIP_JAD_1980_2017.csv?dl=1",stringsAsFactors=FALSE,row.names=NULL)
+JAD<-read.csv("https://www.dropbox.com/s/1w5wlmur5x3x7gq/TRIP_JAD_1980_2017.csv?dl=1",stringsAsFactors=TRUE,row.names=NULL)
+JAD_copy<-JAD
 
 ## reorder variables ##
 JAD<-JAD[c(1:10,23:25,36:38,11:22,26:35,39:97)]
 JAD<-JAD[-c(2,3,4,7,8,9)]
 
-## rename variables ##
+#### rename variables ####
 names(JAD)<-c("pubID","journal","year","Paradigm","Ideational","Material","Epistemology","Contemporary","PolicyPrescription","IssueArea","Realism Taken Seriously","Liberalism Taken Seriously","Marxism Taken Seriously","Constructivism Taken Seriously","Non-paradigmatic Taken Seriously","Atheoretic/None Taken Seriously","Realism Synthesis","Liberalism Synthesis","Marxism Synthesis","Constructivism Synthesis","Non-paradigmatic Synthesis","No Synthesis","Pre-history to 476 AD","476 AD to October 1648","October 1648 to June 28, 1914","June 28, 1914 to June 28, 1919","June 28, 1919 to September 1, 1939","September 1, 1939 to August 1945","September 1945 to November 9, 1989","November 9, 1989 to September 1, 2001","September 1, 2001 to Present","No time period","Level one","Level two","Level three", "No Level","Analytic/non-formal","Counterfactual","Descriptive","Experimental","Formal Modeling","Policy analysis","Qualitative","Quantitative","Antarctica","Canada/Western Europe","East Asia","FSU / Eastern Europe","Global","Latin America","Middle East / North Africa","No region","Oceania","South Asia","Southeast Asia","Subsaharan Africa","United States","Alliances","Balance of Power","Bargaining Deterrence Strategy","Development","Diplomacy","Domestic Politics","Economic Interdependence","Environment","Ethnicity/Religion","Foreign Aid","Foreign Policy","Gender","Humanitarian","Intergovernmental Organization","Interstate Crisis","Interstate War","International Law","Intrastate Conflict","Discipline of IR","International Regimes","Migration","Monetary Policy","Non-governmental Organizations","North-South Relations","Public Health","Public Opinion","Regime Type","Regional Integration","Sanctions","Terrorism","Trade","Weapons Systems","Weapons of Mass Destruction Proliferation","Other")
 
 ## rewrite JAD yes/no ##
@@ -21,36 +26,43 @@ for(i in 11:91){
   i<-i+1
 }
 
-## gather data set ##
+#### gather data set ####
 JAD <- JAD %>% 
   gather(TimePeriod, TimePeriod_value,`Pre-history to 476 AD`:`No time period`) %>% 
-  filter(TimePeriod_value!="") %>%
+filter(TimePeriod_value!="") %>% select(-TimePeriod_value) %>% 
   gather(Level,Level_value,`Level one`:`No Level`) %>% 
-  filter(Level_value!="") %>%
+  filter(Level_value!="") %>% select(-Level_value) %>%
   gather(Focus,Focus_value,`Alliances`:`Other`) %>% 
-  filter(Focus_value!="") %>%
+  filter(Focus_value!="") %>% select(-Focus_value) %>%
   gather(Seriously,Seriously_value,`Realism Taken Seriously`:`Atheoretic/None Taken Seriously`) %>% 
-  filter(Seriously_value!="") %>%
+  filter(Seriously_value!="") %>% select(-Seriously_value) %>%
   gather(Synthesis,Synthesis_value,`Realism Synthesis`:`No Synthesis`) %>% 
-  filter(Synthesis_value!="") %>%
+  filter(Synthesis_value!="") %>% select(-Synthesis_value) %>%
   gather(Methodology,Methodology_value,`Analytic/non-formal`:`Quantitative`) %>% 
-  filter(Methodology_value!="") %>%
+  filter(Methodology_value!="") %>% select(-Methodology_value) %>%
   gather(Region,Region_value,`Antarctica`:`United States`) %>% 
-  filter(Region_value!="") %>%
+  filter(Region_value!="") %>% select(-Region_value) %>%
   select(pubID,journal,year,Paradigm,Epistemology,IssueArea,Ideational,Material,Contemporary,PolicyPrescription,TimePeriod,Level,Focus,Seriously,Synthesis,Methodology,Region)
 
 JAD$TimePeriod <- factor(JAD$TimePeriod, levels=c("Pre-history to 476 AD","476 AD to October 1648","October 1648 to June 28, 1914","June 28, 1914 to June 28, 1919","June 28, 1919 to September 1, 1939","September 1, 1939 to August 1945","September 1945 to November 9, 1989","November 9, 1989 to September 1, 2001","September 1, 2001 to Present","No time period"))
-
 JAD$Level<-factor(JAD$Level,levels=c("Level one","Level two","Level three","No level"))
 
-## set up plot theme ##
+#### set up plot theme ####
 plot_theme<-theme_bw() +
   theme(axis.text.x=element_text(angle=50), axis.title.x=element_text(vjust=1.5),axis.title.y=element_text(hjust=1.5))
+
 
 ######################################################################################
 #                                    USER INTERFACE                                  #
 ######################################################################################
 ui <- fluidPage(
+  tags$head(
+    tags$style(HTML("
+                    .well{
+                        background-color:#e1eaf0;"
+      
+    ))
+  ),
   titlePanel(h1(strong("Journal Article Database")),
              windowTitle="TRIP Journal Article Database"),
   br(),
@@ -60,7 +72,7 @@ ui <- fluidPage(
              tags$div(id="yearFilter",
                       h4("Show by:"),
              radioButtons("percentInput","Frequency or Percentage",choices=c("Frequency","Percentage"),inline=TRUE),
-             radioButtons("ygInput","Year or Variable",c("Year","Variable"),inline=TRUE))),
+             radioButtons("ygInput","X-axis: Year or Variable",c("Year","Variable"),inline=TRUE))),
            wellPanel(
              tags$div(id="newFilter",
                       h4("See all articles which are:"),
@@ -68,18 +80,22 @@ ui <- fluidPage(
                       uiOutput("epistFilter"),uiOutput("contempFilter"),uiOutput("focusFilter"),uiOutput("ideaFilter"),uiOutput("issueFilter"),uiOutput("levelFilter"),uiOutput("materialFilter"),uiOutput("methodFilter"),uiOutput("paradigmFilter"),uiOutput("policyFilter"),uiOutput("regionFilter"),uiOutput("timeFilter"))),
            wellPanel(
              h4("Article year range:"),
-             sliderInput("yearInput","Year",1980,2017,c(1980,2017),sep=""))), #change when updated),
+             sliderInput("yearInput","Year",1980,2017,c(1980,2017),sep="")), #change when updated),
+           wellPanel(
+             h4("Downloaded your selected data set:"),
+             downloadButton("download","Download"),
+             strong(a(actionButton("tweet","Tweet", style="font-style:bold; background-color:#fff; background:url('https://trip.wm.edu/charts/downloads/tweet.png'); width:100px; height:30px; background-size:30px 30px; background-repeat:no-repeat; float:right;"), href="http://twitter.com/intent/tweet?text=Check%20out%20this%20data%20from%20the%20TRIP%20Project!&url=https://trip.wm.edu/data/jad-test"))
+           )),
     column(8,verticalLayout(
-             plotlyOutput("plotly"),fluid=TRUE))),
+             tableOutput("view"),
+             plotlyOutput("plotly")))),
   tags$style(type="text/css", ".shiny-output-error{visibility: hidden;}", ".shiny-output-error:before{visibility:hidden;}"))
-
-
 
 #######################################################################################
 #                                       SERVER                                        #
 #######################################################################################
 server <- function(input, output, session) {
-  ## add new filter button ##  
+  #### add new filter button ####  
   observeEvent(input$addFilter, {
     if(input$addFilter=="Paradigm") {
       output$paradigmFilter<-renderUI({
@@ -132,14 +148,14 @@ server <- function(input, output, session) {
     }
   })
   
-  ## change x-axis filters ##
+  #### change x-axis filters ####
   observeEvent(input$ygInput, { 
     if(input$ygInput=="Variable"){
       insertUI(
         selector="#ygInput",
         where="afterEnd",
         ui=tags$div(id="varFilter",
-                    selectInput("varFilter","X-Axis", c("Journal"="journal","Paradigm","Epistemology","Issue Area"="IssueArea","Time Period"="TimePeriod","Level","Methodology","Region","Substantive Focus"="Focus","Policy Prescription"="PolicyPrescription"))))
+                    selectInput("varFilter","Variable", c("Journal"="journal","Paradigm","Epistemology","Issue Area"="IssueArea","Time Period"="TimePeriod","Level","Methodology","Region","Substantive Focus"="Focus","Policy Prescription"="PolicyPrescription"))))
     } else {
       removeUI(selector="#varFilter")
     }
@@ -169,7 +185,7 @@ server <- function(input, output, session) {
     
     ## manipulate data for plotting ##
     if(input$percentInput=="Frequency" & input$ygInput=="Variable"){
-      data<-data %>% group_by_(input$varFilter) %>% mutate(filter=n_distinct(pubID)) %>% mutate(filter_n=n()) # find unique articles in x-axis #
+      data<-data %>% group_by(input$varFilter) %>% mutate(filter=n_distinct(pubID)) %>% mutate(filter_n=n()) # find unique articles in x-axis #
       data<-data %>% mutate(Count=filter/filter_n)}
     if(input$percentInput=="Frequency" & input$ygInput=="Year"){
       data_n<-data %>% group_by(year, pubID) %>% count(pubID) # create counts of pubID #
@@ -180,50 +196,89 @@ server <- function(input, output, session) {
       data<-data %>% group_by(year) %>% mutate(year_filtered=n())  # find number of all observations in a year #
       data<-data %>% mutate(Percentage=((distinct_filtered/year_filtered)/year_distinct)*100)}  # create percentages to sum for stat_summary #
     if(input$percentInput=="Percentage" & input$ygInput=="Variable"){  # varFilter percentages #     
-      data<-data %>% group_by_(input$varFilter) %>% mutate(new_n=n_distinct(pubID)) %>% mutate(filter_n=n())  # find how articles in x-axis #
+      data<-data %>% group_by(input$varFilter) %>% mutate(new_n=n_distinct(pubID)) %>% mutate(filter_n=n())  # find how articles in x-axis #
       data<-data %>% ungroup() %>% mutate(filter=n_distinct(pubID))# find unique articles #
       data<-data %>% mutate(Percentage=(new_n/filter/filter_n)*100)} # create actual percentages to sum for stat_summary #
     
     data<-as.data.frame(data)
   })
   
-  output$view<-renderText({
-    text<-paste("Total number of articles selected:", filtered() %>% summarize(n=n_distinct(pubID)))
-    text
-  })
-  
-  ## create plot ##
-  output$plotly <- renderPlotly({
-    if(input$percentInput=="Percentage" & input$ygInput=="Variable"){ 
-      p<-ggplot(filtered(), aes_string(x=input$varFilter)) +
+  #### create plot ####
+  plot<-reactive({
+    p<-if(input$percentInput=="Percentage" & input$ygInput=="Variable"){
+      ggplot(filtered(), aes_string(x=input$varFilter)) +
         stat_summary(aes(y=Percentage),fun.y="sum",geom="bar",position="identity",fill="#a2c6da") +
+        scale_x_discrete(drop=FALSE) +
         ylab("Percentage") +
         plot_theme 
-    } else if (input$percentInput=="Frequency" & input$ygInput=="Variable") {
-      p<-ggplot(filtered(), aes_string(x=input$varFilter)) +
-        stat_summary(aes(y=Count),fun.y="sum",geom="bar",position="identity",fill="#a2c6da") +
+    } else if (input$percentInput=="Frequency" & input$ygInput=="Variable"){
+      ggplot(filtered(), aes_string(x=input$varFilter)) +
+        geom_bar(stat="summary",aes(y=Count),fun.y="sum",fill="#a2c6da") +
+        scale_x_discrete(drop=FALSE) +
         ylab("Frequency") +
         plot_theme 
-    } else if (input$percentInput=="Percentage" & input$ygInput=="Year") {
-      p<-ggplot(filtered(), aes(x=year)) +
+    } else if (input$percentInput=="Percentage" & input$ygInput=="Year"){
+      ggplot(filtered(), aes(x=year)) +
         stat_summary(aes(y=Percentage),fun.y="sum",geom="bar",position="identity",fill="#a2c6da") +
+        scale_x_discrete(drop=FALSE) +
         scale_x_continuous(breaks=seq(1980,2017,5),labels=seq(1980,2017,5)) +
         ylab("Percentage") +
         xlab("Year") +
         plot_theme
-    } else { # frequency by year #
-      p<-ggplot(filtered(), aes(x=year)) +
+    } else{ # frequency by year #
+      ggplot(filtered(), aes(x=year)) +
         stat_summary(aes(y=Count),fun.y="sum",geom="bar",position="identity",fill="#a2c6da") +
+        scale_x_discrete(drop=FALSE) +
         scale_x_continuous(labels=seq(1980,2017,5),breaks=seq(1980,2017,5)) +
         xlab("Year") +
         ylab("Frequency") +
         plot_theme
     }
-    
-    ggplotly(p, height=700) %>%
-      layout(margin=list(b=300,l=100),autosize=T,yaxis=list(hoverformat=".3f%"))
+  p
   })
   
+  dash_plot<-reactive({ggplotly(plot(), height=700) %>%
+      layout(margin=list(b=300,l=100),autosize=T,yaxis=list(hoverinfo="p"))})
+  temp_plot<-reactive({ggplotly(plot(), height=575,width=325) %>%
+      layout(margin=list(b=300,l=100),autosize=T,yaxis=list(hoverinfo="p"))})
+  
+  
+    
+  output$plotly <- renderPlotly({ 
+    dash_plot()
+  })
+  
+  #### create and tweet temp image ####
+  observeEvent(input$tweet, {
+    temp_file<-tempfile(tmpdir=tempdir(),fileext=".png")
+    #saveWidget(isolate(temp_plot()), temp_file, selfcontained=TRUE)
+    ggsave(temp_file, isolate(plot()), "png", width=4.6, height=2.4, dpi=100)
+    list(src=temp_file, contentType="png")
+    temp_file
+  })
+  
+  #### test printout #### 
+  output$view<-renderText({}) 
+  ########
+  
+  #### download csv file ####
+  pubID<-reactive({data.frame(as.integer(names(table(filtered()$pubID))))[[1]]})
+  csvDownload<-reactive({
+    data<-JAD_copy
+    data<-data[data$pubID %in% pubID(),]
+    data<-as.data.frame(data)
+  })
+  
+  output$download<-downloadHandler(filename=function(){
+      paste("TRIP_JAD_", format(Sys.Date(), "%Y_%b_%d"), ".csv",sep="")
+    },
+    content=function(file){
+      write.csv(csvDownload(), file, row.names=FALSE)
+    })
+  
+  ########
 }
 
 shinyApp(ui = ui, server = server)
+
+
